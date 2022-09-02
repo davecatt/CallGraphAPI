917b22c8532c611fa39975791bfa5c5235c11381
davecatt <github@davecatt.co.uk>
2022-09-02 08:40:47 +0100

    Add project files.

---
+++ CallGraphAPI/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace CallGraphAPI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+    public partial class App : Application
+    {
+        static App()
+        {
+            // AzureCloudInstance.AzurePublic resolves to "https://login.microsoftonline.com/"
+            // For a desktop app using .NET Framework, the redirect should be https://login.microsoftonline.com/common/oauth2/nativeclient
+            //      The application will open a popup login screen from Azure AD.
+            // For a UWP it should be value of WebAuthenticationBroker.GetCurrentApplicationCallbackUri()
+            // For a .NET Core app it should be http://localhost so the user goes through a browser for interactive authentication since .NET Core doesn't have a UI for the embedded web view at the moment.
+            //      For browsers in desktop scenarios the redirect uri is intercepted by MSAL to detect that a response is returned from the identity provider.
+            // This uri can therefore be used in any cloud without seeing an actual redirect to that uri.  This means you can and should use https://login.microsoftonline.com/common/oauth2/nativeclient in any cloud.
+            // If you prefer you can also translate this to another uri as long as you configure the redirect uri correctly with MSAL.
+            // Specifying the above in the application registration means there is the least amount of setup in MSAL.
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .WithRedirectUri("http://localhost")
+                //.WithDefaultRedirectUri()
+                .Build();
+        }
+
+        //   - For Work or School account in your org, use your tenant ID, or domain
+        //   - for any Work or School accounts, use `organizations`
+        //   - for any Work or School accounts, or Microsoft personal account, use `common`
+        //   - for Microsoft Personal account, use 'consumers'
+        private static string Tenant = "organizations";
+
+        private static IPublicClientApplication _clientApp;
+        public static IPublicClientApplication PublicClientApp { get { return _clientApp; } }
+        private static string ClientId = "60252733-a707-4e1b-aaf3-87275633656b";
+    }
+}
---
+++ CallGraphAPI/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Identity.Client;
+
+
+// Based on https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-windows-desktop
+
+namespace CallGraphAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Set the API Endpoint to Graph 'me' endpoint
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call
+        string[] scopes = new string[] { "user.read", "api://a2b5731a-397e-4a66-9b8f-7ec5989e3bb4/World.Domination" };
+        //string[] scopes = new string[] { "user.read", "People.Read.All" };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// Call AcquireToken - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            ResultText.Text = string.Empty;
+            TokenInfoText.Text = string.Empty;
+
+            var accounts = await app.GetAccountsAsync();
+            var firstAccount = accounts.FirstOrDefault();
+
+            // Try to obtain a token silently first, in case the user is already authenticated
+            try
+            {
+                authResult = await app.AcquireTokenSilent(scopes, firstAccount)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent which indicates we need to call AcquireTokenInteractive
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(scopes)
+                        .WithAccount(accounts.FirstOrDefault())
+                        .WithPrompt(Prompt.SelectAccount)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                // Check to see if the sign-in is from a non-subscribed tenant (this is Microsoft - change this for a tenant that you want to exclude)
+                if (authResult.TenantId == "72f988bf-86f1-41af-91ab-2d7cd011db47")
+                {
+                    ResultText.Text = "The tenant does not have a subscription to this service";
+                    DisplayBasicTokenInfo(authResult);
+                    this.SignOutButton.Visibility = Visibility.Visible;
+                }
+
+                else
+                {
+                    ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+
+                    // if you wish to display the whole token, uncomment the line below and then copy and paste the encoded token into a website such as https://jwt.ms/
+                    //ResultText.Text =  authResult.AccessToken;
+                    DisplayBasicTokenInfo(authResult);
+                    this.SignOutButton.Visibility = Visibility.Visible;
+                }
+
+            }
+        }
+
+        // Perform an HTTP GET request to a URL using an HTTP Authorization header
+        // <param name="url">The URL</param>
+        // <param name="token">The token</param>
+        // <returns>String containing the results of the GET operation</returns>
+        public async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                //Add the token in Authorization header
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        // Display basic information contained in the token
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Username: {authResult.Account.Username}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+            }
+        }
+
+        // Sign out the current user
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var accounts = await App.PublicClientApp.GetAccountsAsync();
+
+            if (accounts.Any())
+            {
+                try
+                {
+                    await App.PublicClientApp.RemoveAsync(accounts.FirstOrDefault());
+                    this.ResultText.Text = "User has signed-out";
+                    this.CallGraphButton.Visibility = Visibility.Visible;
+                    this.SignOutButton.Visibility = Visibility.Collapsed;
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            }
+        }
+    }
+}
